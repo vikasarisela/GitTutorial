5d791bdaa7015cdc1293b39b300a86aacbee668e
Vikas Arisela <vikasarisela@Vikass-MacBook-Air.local>
2018-02-02 12:37:51 +0530

    changes done in map view branch

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         print("Hello")
         print("changed")
+        print("Pulled and changed")
+        print("Branch from mapView")
     }
 
     override func didReceiveMemoryWarning() {
